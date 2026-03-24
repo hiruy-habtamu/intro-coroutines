7f14319c6e60c535aacfe592505de12eaa144105
Hiruy <hiruyhabtamu12@gmail.com>
2026-03-24 19:25:53 +0300

    Finished Task 4

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -1,7 +1,22 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+
+    val repos =
+        service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+    return repos.flatMap { repo ->
+        service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+    }.aggregate()
+
 }
