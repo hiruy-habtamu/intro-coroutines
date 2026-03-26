d28369594d5d24de20ca50473f5d30ec4eb23649
Hiruy <hiruyhabtamu12@gmail.com>
2026-03-26 12:10:28 +0300

    Finished Task 6

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,31 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+
+    val repos =
+        service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+    // Using mutableList here instead of making an immutable List is a bad idea in the case of concurrency
+    // Things like race conditions are bound to happen as mutable functions such as MutableList<T>.add() ARE NOT ATOMIC!
+    val users: MutableList<User> = emptyList<User>().toMutableList()
+
+    repos.withIndex().forEach { (index, repo) ->
+        val repoUsers = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        users.addAll(repoUsers)
+        updateResults(users.aggregate(), index == repos.lastIndex)
+    }
 }
